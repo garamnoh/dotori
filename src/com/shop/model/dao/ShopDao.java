8e43ca07ca976d3ec2ea09d994cd72a537b47c22
yhsin79 <yhsin79@naver.com>
2021-05-27 23:59:41 +0900

    중간 저장용 커밋입니다 아직 뮤직은 구현 안했습니다

---
+++ src/com/shop/model/dao/ShopDao.java
@@ -0,0 +1,56 @@
+package com.shop.model.dao;
+
+import static com.common.JDBCTemplate.close;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.shop.model.vo.MusicItem;
+
+
+
+public class ShopDao {
+private Properties prop=new Properties();
+	
+	public ShopDao() {
+		String path=ShopDao.class.getResource("/sql/shop_sql.properties").getPath();
+		try {
+			prop.load(new FileReader(path));
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public List<MusicItem> musicList(Connection conn){
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<MusicItem> list=new ArrayList();
+		try {
+			pstmt=conn.prepareStatement(prop.getProperty("musicList"));
+			
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				MusicItem m=new MusicItem();
+				m.setMusicNo(rs.getInt("musie_no"));
+				m.setMusicTitle(rs.getString("musie_title"));
+				m.setSinger(rs.getString("singer"));
+				m.setFilePath(rs.getString("filepath"));
+				m.setPrice(rs.getInt("price"));
+				list.add(m);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}return list;
+	}
+
+}
