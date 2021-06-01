e4af1fc1cdff464e08b4dc8236693d0b210d6fbf
yhsin79 <yhsin79@naver.com>
2021-06-01 18:09:00 +0900

    제바라라라라라

---
+++ src/com/page/controller/ContentToShopBasketServlet.java
@@ -0,0 +1,44 @@
+package com.page.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContentToShopBasketServlet
+ */
+@WebServlet("/page/option3.do")
+public class ContentToShopBasketServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ContentToShopBasketServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		System.out.println("testestestes");
+		request.getRequestDispatcher("/views/shop/shopBasket.jsp").forward(request,response);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
